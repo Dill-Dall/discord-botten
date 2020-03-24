16a6dc1fb10fb3256e2a906465f6a41925d20b28
Lai <laialexanderholmsen@gmail.com>
2020-03-24 13:59:27 +0100

    Refactor to comply with pep8

--- diceroller.py
+++ diceroller.py
@@ -2,26 +2,26 @@ import string
 import random
 
 async def roll(ctx, *args):
-    diceString = ''.join(args)
-    dice = diceString.translate({ord(c): None for c in string.whitespace})
+    dice_strings = ''.join(args)
+    dice = dice_strings.translate({ord(c): None for c in string.whitespace})
 
     #[2d4,4d8]
-    diceSets = dice.split("+")
-    diceStrings = []
+    dice_sets = dice.split("+")
+    dice_strings = []
     totalsum = 0
     #[[2,4],[4,8]]
-    for dice_set in diceSets:
+    for dice_set in dice_sets:
         dice_set = dice_set.split("d")
         dice_set = [str(random.choice(range(1, int(dice_set[1])+ 1))) for _ in range(int(dice_set[0]))]
-        sumOfDice = sum([int(i) for i in dice_set])
-        totalsum += sumOfDice
-        diceValues = ','.join(dice_set)
-        diceStrings.append(f'{diceValues}={sumOfDice}')
+        sum_of_dice = sum([int(i) for i in dice_set])
+        totalsum += sum_of_dice
+        dice_values = ','.join(dice_set)
+        dice_strings.append(f'{dice_values}={sum_of_dice}')
 
-    responseString=''
-    for s in diceStrings:
-        responseString += f'{s} : '
+    response_string = ''
+    for dice_string in dice_strings:
+        response_string += f'{dice_string} : '
 
-    await ctx.send(f">>> {responseString} = {totalsum}")
+    await ctx.send(f">>> {response_string} = {totalsum}")
 
 players = {}
