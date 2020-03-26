a6e65f95c95568b5004130fe9c2223418ad060a7
Lai <laialexanderholmsen@gmail.com>
2020-03-26 14:45:28 +0100

    Refactor roll(). Beautify output.

--- diceroller.py
+++ diceroller.py
@@ -1,7 +1,6 @@
 """
     Module to handle rolling of dice
 """
-import string
 import random
 
 from discord.ext import commands
@@ -10,27 +9,30 @@ class Diceroller(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command(help='!roll 3d4 could give 3,1,2 = 6.. !roll 2d20 +3d10, could give "18,17=35 : 8,1,6=15 :  = 50"')
+    # TODO 
+    # Update help text, possible make it a dosctring instead
+    @commands.command(help='!roll 3d4 could give 3,1,2 = 6.. !roll 2d20 3d10, could give "18,17=35 : 8,1,6=15 :  = 50"')
     async def roll(self, ctx, *args):
-        dice_strings = ''.join(args)
-        dice = dice_strings.translate({ord(c): None for c in string.whitespace})
+        dice = args
+        total_sum = 0
+        full_response = []
 
-        #[2d4,4d8]
-        dice_sets = dice.split("+")
-        dice_strings = []
-        totalsum = 0
-        #[[2,4],[4,8]]
-        for dice_set in dice_sets:
-            dice_set = dice_set.split("d")
-            dice_set = [str(random.choice(range(1, int(dice_set[1])+ 1))) for _ in range(int(dice_set[0]))]
-            sum_of_dice = sum([int(i) for i in dice_set])
-            totalsum += sum_of_dice
-            dice_values = ','.join(dice_set)
-            dice_strings.append(f'{dice_values}={sum_of_dice}')
+        for die in dice:
+            number_of_die = int(die.split("d")[0])
+            die_value = int(die.split("d")[1])
+            dice_values = [str(random.randint(1, die_value)) for i in range(number_of_die)]
+            sum_of_dice = sum(int(i) for i in dice_values) 
+            total_sum += sum_of_dice
 
-        response_string = ''
-        for dice_string in dice_strings:
-            response_string += f'{dice_string} : '
-        await ctx.send(f">>> {response_string} = {totalsum}")
+            if number_of_die == 1:
+                response = f"{die} : {sum_of_dice}"
+            else:
+                response = f"{die}: {'+'.join(dice_values)} = {sum_of_dice}"
+            full_response.append(response)
 
-    # players = {} # why is this here?
+        if len(full_response) > 1:
+            # Can't have backslash inside expression in an f-string.
+            # "chr(10)" gets the character that represents unicode 10, which is "\n"
+            await ctx.send(f">>> {chr(10).join(full_response)}\nTotal sum = {total_sum}")
+        else:
+            await ctx.send(f">>> {''.join(full_response)}")
